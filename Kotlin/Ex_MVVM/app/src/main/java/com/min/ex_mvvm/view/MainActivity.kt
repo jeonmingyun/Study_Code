da85c2a7c175ec6c117e8aba793c0a1e0ecbb7d0
jeonmingyun <jun.min.gyun@gmail.com>
2022-03-14 01:49:25 +0900

    livedata test code

--- Kotlin/Ex_MVVM/app/src/main/java/com/min/ex_mvvm/view/MainActivity.kt
+++ Kotlin/Ex_MVVM/app/src/main/java/com/min/ex_mvvm/view/MainActivity.kt
@@ -1,36 +1,43 @@
 package com.min.ex_mvvm.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.min.ex_mvvm.R
 import com.min.ex_mvvm.databinding.ActivityMainBinding
 import com.min.ex_mvvm.model.User
 import com.min.ex_mvvm.view_model.MyViewModel
 
 class MainActivity : AppCompatActivity() {
-    private var mBinding : ActivityMainBinding? = null
+    private var mBinding: ActivityMainBinding? = null
     private val binding get() = mBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // viewModel 연결
         val model: MyViewModel = ViewModelProvider(this).get(MyViewModel::class.java)
-        model.getUsers().observe(this, Observer<List<User>>{ users ->
-            // update UI
-            if(users != null) {
-                for (user:User in users) {
-                    Log.e("ddddddd", user.id);
-                    binding.idEditText.setText(user.id)
-                }
-            }
+        binding.myViewModel = model
 
+
+        // Create the observer which updates the UI.
+        // 변수명, 람다식 파라미터명 모두 자동생성되기 때문에 viewModel의 변수 명과 맞춤
+        val resultObserver = Observer<Int> { result ->
+            // Update the UI
+            binding.calEditText.setText(result.toString())
+        }
+
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        model.result.observe(this, resultObserver)
+
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        model.user.observe(this,  Observer<User> { user ->
+            // Update the UI
+            binding.idEditText.setText(user.id)
+            binding.pwEditText.setText(user.pw)
         })
     }
 
--- Kotlin/Ex_MVVM/app/src/main/java/com/min/ex_mvvm/view_model/MyViewModel.kt
+++ Kotlin/Ex_MVVM/app/src/main/java/com/min/ex_mvvm/view_model/MyViewModel.kt
@@ -1,6 +1,5 @@
 package com.min.ex_mvvm.view_model
 
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,16 +7,18 @@ import com.min.ex_mvvm.model.User
 
 class MyViewModel : ViewModel() {
     // 수정 가능한 데이터
-    private val users: MutableLiveData<List<User>> by lazy {
-        MutableLiveData<List<User>>().also {
+    val user: MutableLiveData<User> by lazy {
+        MutableLiveData<User>().also {
+            
             loadUsers()
         }
     }
-      val _result =  MutableLiveData(0)
+
+    val result =  MutableLiveData(0)
 
     // Read only 데이터
-    fun getUsers(): LiveData<List<User >> {
-        return users
+    fun getUsers(): LiveData<User> {
+        return user
     }
 
     private fun loadUsers() {
@@ -26,10 +27,10 @@ class MyViewModel : ViewModel() {
     }
 
     fun increase() {
-        _result.value = (_result.value?: 0) + 1
+        result.value = (result.value?: 0) + 1
     }
 
     fun decrease() {
-        _result.value = (_result.value?: 0) - 1
+        result.value = (result.value?: 0) - 1
     }
 }
