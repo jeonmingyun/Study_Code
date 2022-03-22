2a908d16c3d38c95b9fd19b71211b9ba1faf5e66
jun.min.gyun@gmail.com <jun.min.gyun@gmail.com>
2022-03-22 18:56:09 +0900

    create project

---
+++ JAVA/project_map/app/src/main/java/com/min/project_map/MainAct.java
@@ -0,0 +1,14 @@
+package com.min.project_map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+}
