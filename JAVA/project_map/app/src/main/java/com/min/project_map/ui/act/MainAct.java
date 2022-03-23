25d4806242ddcede1be78320678f4b9f28e6b664
jun.min.gyun@gmail.com <jun.min.gyun@gmail.com>
2022-03-23 18:18:51 +0900

    create view

--- JAVA/project_map/app/src/main/java/com/min/project_map/ui/act/MainAct.java
+++ JAVA/project_map/app/src/main/java/com/min/project_map/ui/act/MainAct.java
@@ -1,8 +1,10 @@
-package com.min.project_map;
+package com.min.project_map.ui.act;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.min.project_map.R;
 
 public class MainAct extends AppCompatActivity {
 
